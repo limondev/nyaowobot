4628b0f2ec4ab9bf1c96621843f9006294546475
limondev <73124097+limondev@users.noreply.github.com>
2023-12-30 11:58:06 +0200

    NyaBot v1.1
    Added /weather for current weather state in your city. Also some bugfixes.

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import telebot
 import random
-from config import TELEGRAM_API_TOKEN
+import requests
+
+
+from config import TELEGRAM_API_TOKEN, api_key
 bot = telebot.TeleBot(TELEGRAM_API_TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
-def send_kawaii_instructions(message): # Це тіпа для старту
+def send_kawaii_instructions(message):
     instructions = (
         "Konnichiwa~! To make your message kawaii, simply type /kawaii followed by your message. "
         "For example: /kawaii Hello, how are you? UwU"
+        "Also, U can ask this bot for weather in your city, just type /weather followed by mane of your city :3"
+        "For example: /weather Kharkiv"
     )
     bot.reply_to(message, instructions)
 
@@ -34,6 +39,34 @@ def make_kawaii(user_message: str):
     kawaii_message = user_message + " " + random_string
     return kawaii_message
 
-bot.polling(none_stop=True, interval=0)
+@bot.message_handler(commands=['weather'])
+def get_weather(message):
+    try:
+        city = message.text.split('/weather ', 1)[1]
+        url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+        response = requests.get(url)
+        weather_data = response.json()
 
+        if 'main' in weather_data and 'weather' in weather_data:
+            main_weather = weather_data['weather'][0]['main']
+            description = weather_data['weather'][0]['description']
+
+            temperature_kelvin = weather_data['main']['temp']
+            feels_like_kelvin = weather_data['main']['feels_like']
+
+            temperature_celsius = temperature_kelvin - 273.15
+            feels_like_celsius = feels_like_kelvin - 273.15
+
+            bot.reply_to(message, f'The weather in {city} is {main_weather} ({description}) Nya!\n'
+                                  f'Temperature: {temperature_celsius:.2f}°C! :3\n'
+                                  f'Feels like: {feels_like_celsius:.2f}°C! UwU')
+        else:
+            bot.reply_to(message, f'Sorry, I couldn\'t retrieve the weather information for {city}. Nya~ :(')
+    except IndexError:
+        bot.reply_to(message, 'Please provide a city name after the /weather command. UwU')
+    except Exception as e:
+        bot.reply_to(message, f'Something went wrong: {str(e)}. OwO')
+
+if __name__ == "__main__":
+    bot.polling(none_stop=True, interval=0)
 
